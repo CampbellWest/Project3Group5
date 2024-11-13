ed7c49b0806bcdbd1815a94c4b5f30b8fc2a6e17
CampbellWest <campbellwest1@gmail.com>
2024-11-12 19:19:06 -0500

    Commit

--- HomeMonitoringSystem/main.cpp
+++ HomeMonitoringSystem/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <qdebug.h> // this is so you can print things to the console when debugging
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    qDebug() << "ERROR"; // like so
+
     return a.exec();
 }
